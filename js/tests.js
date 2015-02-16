d9fbfd8f14accc11759dccff73bbf798a153942e
Adam Burucs <adam.burucs@gmail.com>
2015-02-16 14:54:31 +0100

    unit testing added

--- js/tests.js
+++ js/tests.js
@@ -1,1 +1,9 @@
 /* tests */
+
+QUnit.test('Below 19', function (assert) {
+    assert.equal(convertNumeral(0), "zero", 'zero');
+    assert.equal(convertNumeral(7), "seven", 'seven');
+    assert.equal(convertNumeral(11), "eleven", 'eleven');
+    assert.equal(convertNumeral(12), "twelve", 'twelve');
+    assert.equal(convertNumeral(19), "nineteen", 'nineteen');
+});
