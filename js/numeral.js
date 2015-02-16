fe5c147c723287d194ad321b1d50558463274cd0
Adam Burucs <adam.burucs@gmail.com>
2015-02-16 14:05:26 +0100

    first version
    it works between 0-99

--- js/numeral.js
+++ js/numeral.js
@@ -1,7 +1,47 @@
 /* numeral */
 
-function convertNumeral(num) {
-    return "zero";
+function convertNumeral(number) {
+    var words = "";
+    
+    var unitsMap = [
+        "zero", "one", "two", "three",
+        "four", "five", "six", "seven",
+        "eight", "nine", "ten", "eleven",
+        "twelve", "thirteen", "fourteen",
+        "fifteen", "sixteen", "seventeen",
+        "eighteen", "nineteen"
+    ];
+    
+    var tensMap = [
+        "zero", "ten", "twenty", "thirty",
+        "forty", "fifty", "sixty", "seventy",
+        "eighty", "ninety"
+    ];
+
+    if (number >= 0 && number <= 19) {
+        words = unitsMap[number];
+    }
+    
+    if (number >= 20 && number <= 99) {
+        var digit1 = parseInt(number.toString()[0]);
+        var digit2 = parseInt(number.toString()[1]);
+        
+        if (digit2 == 0) {
+            words = tensMap[digit1];
+        } else {
+            words = tensMap[digit1] + "-" + unitsMap[digit2];
+        }
+    }
+
+    if (number >= 100 && number <= 999) {
+        /*
+        var digit1 = parseInt(number.toString()[0]);
+        var digit2 = parseInt(number.toString()[1]);
+        var digit3 = parseInt(number.toString()[2]);
+        */
+    }
+    
+    return words;    
 }
 
 $(document).ready(function() {
